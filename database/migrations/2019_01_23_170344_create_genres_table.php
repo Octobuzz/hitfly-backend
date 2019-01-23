ee65ff936478b9bda83c811edb68bfc73429d6ea
z.stepcheva <z.stepcheva@itech-group.ru>
2019-01-23 22:48:19 +0400

    add migrations for users, genres, albums, socials, feedbacks, tracks

--- database/migrations/2019_01_23_170344_create_genres_table.php
+++ database/migrations/2019_01_23_170344_create_genres_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGenresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable();
-            $table->string('email', 191)->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->primary('id');
+            
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('genres');
     }
 }
---
+++ database/migrations/2019_01_23_171708_create_albums_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type')->nullable();
+            $table->string('title');
+            $table->string('author');
+            $table->integer('year');
+            $table->string('cover');
+            $table->integer('likes');
+            $table->integer('dislikes');
+            $table->timestamps();
+
+            $table->unsignedInteger('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
