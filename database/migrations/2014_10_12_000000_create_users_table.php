0adbd6ab903f39588eb91c0c6dd9778675cf2ef7
z.stepcheva <z.stepcheva@itech-group.ru>
2019-01-25 14:23:38 +0400

    modified users table for social auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable();
-            $table->string('email', 191)->nullable();
+            $table->string('name');
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->index(['email', 'password']);
+            
             $table->rememberToken();
             $table->timestamps();
         });
