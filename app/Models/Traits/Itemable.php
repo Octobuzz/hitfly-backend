8ae0b3d1161fc404413390ab587304bae4ba7ccb
z.stepcheva <z.stepcheva@zebrains.team>
2019-02-19 17:29:29 +0400

    add migration, model & relations for likes

--- app/Models/Traits/Itemable.php
+++ app/Models/Traits/Itemable.php
@@ -4,11 +4,17 @@ namespace App\Models\Traits;
 
 
 use App\Models\Favourite;
+use App\Models\Like;
 
 trait Itemable
 {
     public function favourites()
     {
         return $this->morphMany(Favourite::class, 'favouriteable');
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
     }
 }
