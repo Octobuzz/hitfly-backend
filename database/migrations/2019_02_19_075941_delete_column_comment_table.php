8df28bfb44f0b00e6fa550602329a836665f4f0c
Dmitrii <d.varava@itech-group.ru>
2019-02-20 13:12:59 +0300

    https://jira.itech-group.ru/browse/GYRE-69 comments Polymorphyc relations(add migration)

---
+++ database/migrations/2019_02_19_075941_delete_column_comment_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteColumnCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn('comment_date');
+            $table->dropForeign('foreign_comments_tracks');
+            $table->dropForeign('foreign_comments_albums');
+        });
+        Schema::table('comments', function (Blueprint $table) {
+            $table->renameColumn('track_id', 'commentable_id');
+            $table->renameColumn('album_id', 'commentable_type');
+        });
+        Schema::table('comments', function (Blueprint $table) {
+            $table->string('commentable_type', 40)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dateTime('comment_date')->nullable(false);
+
+        });
+        Schema::table('comments', function (Blueprint $table) {
+            $table->renameColumn('commentable_id', 'track_id');
+            $table->renameColumn('commentable_type', 'album_id');
+        });
+        Schema::table('comments', function (Blueprint $table) {
+            $table->foreign('track_id', 'foreign_comments_tracks')->references('id')->on('tracks');
+            $table->foreign('album_id', 'foreign_comments_albums')->references('id')->on('albums');
+            $table->integer('album_id')->unsigned()->nullable(true)->change();
+        });
+    }
+}
