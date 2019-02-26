64e115d9b40d65e760660c9abdf63475641bb909
Dmitrii <d.varava@itech-group.ru>
2019-02-26 15:47:59 +0300

    https://jira.itech-group.ru/browse/GYRE-62 policy

--- app/Http/GraphQL/Mutations/CommentMutation.php
+++ app/Http/GraphQL/Mutations/CommentMutation.php
@@ -21,29 +21,37 @@ class CommentMutation
      */
     public function resolve($rootValue, array $args)
     {
-        switch ($args['type']){
-            case Comment::TYPE_ALBUM:
-                $args['commentable_type'] = Album::class;
-                $args['commentable_id'] = $args['id'];
-                break;
-            case Comment::TYPE_TRACK:
-                $args['commentable_type'] = Track::class;
-                $args['commentable_id'] = $args['id'];
-                break;
-            default:
-                throw new \Exception('Не удалось определить тип комментария');
-                break;
+        $user = Auth::user();
+
+//        $user = \App\User::query()->where(['id' => 3])->first();
+
+        var_dump($user->can('update', Comment::class));die();
+        if ($user->can('update', Comment::class)) {
+            switch ($args['type']) {
+                case Comment::TYPE_ALBUM:
+                    $args['commentable_type'] = Album::class;
+                    $args['commentable_id'] = $args['id'];
+                    break;
+                case Comment::TYPE_TRACK:
+                    $args['commentable_type'] = Track::class;
+                    $args['commentable_id'] = $args['id'];
+                    break;
+                default:
+                    throw new \Exception('Не удалось определить тип комментария');
+                    break;
+            }
+
+            $args['user_id'] = Auth::user()->id;
+
+            $comment = new Comment();
+            foreach (Comment::FILLABLE as $fill) {
+                $comment->$fill = $args[$fill];
+            }
+            $comment->save();
+
+            return $comment;
         }
-
-        $args['user_id'] = Auth::user()->id;
-
-        $comment = new Comment();
-        foreach (Comment::FILLABLE as $fill) {
-            $comment->$fill = $args[$fill];
-        }
-        $comment->save();
-
-        return $comment;
+        return false;
     }
 
 
