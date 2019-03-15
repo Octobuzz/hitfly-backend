69b5905f139fdfba66f68e7b572fa53c3c2bb137
g.yuzhakov <g.yuzhakov@zebrains.team>
2019-03-15 12:00:50 +0300

    remove is me

--- app/Http/GraphQL/Type/UserType.php
+++ app/Http/GraphQL/Type/UserType.php
@@ -44,12 +44,6 @@ class UserType extends GraphQLType
                 'type' => \GraphQL::type('GenderType'),
                 'description' => 'GenderType',
             ],
-            // Uses the 'getIsMeAttribute' function on our custom User model
-            'isMe' => [
-                'type' => Type::boolean(),
-                'description' => 'True, if the queried user is the current user',
-                'selectable' => false, // Does not try to query this from the database
-            ],
         ];
     }
 }
