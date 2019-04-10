6cf1facda989e532fa665ced21e53ebe799fc7d0
g.yuzhakov <g.yuzhakov@zebrains.team>
2019-04-10 17:24:16 +0300

    GYRE-187 при заведении пользователя не верный логин

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -68,8 +68,8 @@ class UserController extends \Encore\Admin\Controllers\UserController
 
         $form->display('id', 'ID');
 
-        $form->text('username', trans('admin.username'))->rules('required');
-        $form->text('email', trans('auth.email'))->rules('required');
+        $form->text('username', 'Имя пользователя')->rules('required');
+        $form->text('email', 'Email (логин)')->rules('required');
         $form->image('avatar', trans('admin.avatar'));
         $form->password('password', trans('admin.password'))->rules('confirmed');
         $form->password('password_confirmation', trans('admin.password_confirmation'))
