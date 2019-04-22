bd76508950389c62cff76cad6fd370a678fbc9d7
g.yuzhakov <g.yuzhakov@zebrains.team>
2019-04-22 17:17:12 +0300

    GYRE-188 API - регистрация нового пользователя - отсутствует обязательный параметр "Жанры"

--- app/Http/GraphQL/Mutations/RegisterMutation.php
+++ app/Http/GraphQL/Mutations/RegisterMutation.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\GraphQL\Mutations;
 
+use App\Models\Genre;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -37,7 +38,17 @@ class RegisterMutation extends Mutation
             'gender' => empty($args['user']['gender']) ? '' : $args['user']['gender'],
             'access_token' => Str::uuid(),
         ]);
+
         Auth::guard()->login($user);
+
+        if (!empty($args['profile']['genres'])) {
+            $tmpGenres = [];
+            $genres = Genre::query()->findMany($args['user']['genres']);
+            foreach ($genres as $genre) {
+                $tmpGenres[] = $genre->id;
+            }
+            $user->favouriteGenres()->sync($tmpGenres);
+        }
 
         return $user;
     }
