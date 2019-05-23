2dd2636b45fee9b072402f1cb7c75723f058c307
Dmitrii Varava <d.varava@itech-group.ru>
2019-05-23 18:14:59 +0300

    GYRE-228 сохранение текста песни из файла

--- resources/lang/ru/validation.php
+++ resources/lang/ru/validation.php
@@ -156,6 +156,7 @@ return [
     'oldPasswordIncorrect' => 'Старый пароль неверный',
     'emptyPassword' => 'Пароль неможет быть пустым',
     'emailIsBad' => 'Запрещенный или невалидный email',
+    'invalidFileFormat' => 'Неверный формат файла',
 
 
 ];
