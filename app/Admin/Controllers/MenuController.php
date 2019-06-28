81f638ef9160aaf8265001c569a0e0cd7b0eaea7
d.varava <d.varava@itech-group.ru>
2019-06-28 19:18:36 +0300

    баги админки

--- app/Admin/Controllers/MenuController.php
+++ app/Admin/Controllers/MenuController.php
@@ -99,6 +99,12 @@ class MenuController extends \Encore\Admin\Controllers\MenuController
         $form->display('created_at', trans('admin.created_at'));
         $form->display('updated_at', trans('admin.updated_at'));
 
+        $form->footer(function ($footer) {
+            $footer->disableViewCheck();
+            $footer->disableEditingCheck();
+            $footer->disableCreatingCheck();
+        });
+
         return $form;
     }
 }
