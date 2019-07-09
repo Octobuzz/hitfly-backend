d84a62ce3c4801d91638e4da2e0d73361e2e5d6a
d.varava <d.varava@itech-group.ru>
2019-07-09 19:19:10 +0300

    GYRE-401 fix Location

--- app/Http/GraphQL/Interfaces/UserInterface.php
+++ app/Http/GraphQL/Interfaces/UserInterface.php
@@ -57,7 +57,7 @@ class UserInterface extends InterfaceType
                 'type' => \GraphQL::type('CityType'),
                 'description' => 'Локация пользователя',
                 'alias' => 'city_id',
-                'selectable' => false,
+                //'selectable' => false,
                 'resolve' => function ($model) {
                     return $model->location;
                 },
