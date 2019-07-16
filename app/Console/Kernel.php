42cfbe298f671e0771debbe0674d895c59feff23
g.yuzhakov <g.yuzhakov@zebrains.team>
2019-07-16 20:03:54 +0300

    GYRE-357 Фикс команд

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -63,11 +63,11 @@ class Kernel extends ConsoleKernel
             $this->notification->reachTop(20);
         })->dailyAt('10:00');
         // Создание топ 50 каждый день в 1 час ночи
-        $schedule->call(CreateTopFiftyCommand::class)->dailyAt('01:00');
-        $schedule->call(CalculateListeningTrackCommand::class)->everyTenMinutes();
-        $schedule->call(CalculateListenedUserCommand::class)->everyTenMinutes();
-        $schedule->call(MusicalWaveCommand::class)->everyTenMinutes()->name('create_music_wave')->withoutOverlapping();
-        $schedule->call(ConvertTrackCommand::class)->everyTenMinutes()->name('convert_track')->withoutOverlapping();
+        $schedule->command(CreateTopFiftyCommand::class)->dailyAt('01:00');
+        $schedule->command(CalculateListeningTrackCommand::class)->everyTenMinutes();
+        $schedule->command(CalculateListenedUserCommand::class)->everyTenMinutes();
+        $schedule->command(MusicalWaveCommand::class)->everyFiveMinutes()->name('create_music_wave')->withoutOverlapping();
+        $schedule->command(ConvertTrackCommand::class)->everyFiveMinutes()->name('convert_track')->withoutOverlapping();
     }
 
     /**
