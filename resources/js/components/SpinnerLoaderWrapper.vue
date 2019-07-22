5841314fb1e1d15845495810020ed66f5489cdac
Denis Zharenko <d.zharenko@zebrains.team>
2019-07-22 17:04:06 +0300

    GYRE-354 add public apollo client; add loader wrapper; add header profile popover; add authentication state to store

---
+++ resources/js/components/SpinnerLoaderWrapper.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="spinner-loader-wrapper">
+    <SpinnerLoader
+      v-if="isLoading"
+      class="spinner-loader-wrapper__spinner"
+      v-bind="$attrs"
+      :is-loading="isLoading"
+    />
+    <slot v-else />
+  </div>
+</template>
+
+<script>
+import SpinnerLoader from './SpinnerLoader.vue';
+
+export default {
+  components: {
+    SpinnerLoader
+  },
+  props: {
+    isLoading: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style
+  scoped
+  lang="scss"
+>
+.spinner-loader-wrapper {
+  height: 100%;
+
+  &__spinner {
+    display: block;
+    margin: auto;
+  }
+}
+</style>
