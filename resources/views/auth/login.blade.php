d5f06c7246aacd2814a03665a7ec4a80d005b108
y.zharsky <y.zharsky@zebrains.team>
2019-09-10 10:45:00 +0300

    GYRE-740 | removed special symbols from regexp

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
             @if ($errors->has('password'))
                 <span class="input-text-wrapper__error-msg">{{ $errors->first('password') }}</span>
             @else
-                <span class="input-text-wrapper__error-msg">Пароль должен включать:<br>- буквы латинского алфавита в разных регистрах<br>- цифры<br>- спецсимволы<br>- и быть более 8 символов. Например, MyHitfly1!</span>
+                <span class="input-text-wrapper__error-msg">Пароль должен включать:<br>- буквы латинского алфавита в разных регистрах<br>- цифры<br>- и быть более 8 символов. Например, MyHitfly1</span>
             @endif
         </span>
 
