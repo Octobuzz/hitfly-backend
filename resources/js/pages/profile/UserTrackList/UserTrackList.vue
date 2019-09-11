50e82354e7c0ea63fa43d671d967ed07c5492860
y.zharsky <y.zharsky@zebrains.team>
2019-09-11 17:32:15 +0300

    GYRE-499 | other user id fixed

--- resources/js/pages/profile/UserTrackList/UserTrackList.vue
+++ resources/js/pages/profile/UserTrackList/UserTrackList.vue
@@ -263,7 +263,7 @@ export default {
           .then(response => {
             let data = {
               'type': 'tracks',
-              'id': userId
+              'id': this.currentId
             };
             this.$store.commit('player/pausePlaying');
             this.$store.commit('player/changeCurrentType', data);
