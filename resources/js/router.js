7d468dcb08e124f60c3123825e82633ae837bd60
Denis Zharenko <d.zharenko@zebrains.team>
2019-09-30 15:37:34 +0300

    GYRE-910 change and mount faq page to the main layout

--- resources/js/router.js
+++ resources/js/router.js
@@ -427,6 +427,10 @@ const routes = [
         component: profile.BonusProgram
       },
       {
+        path: 'faq',
+        component: FaqPage
+      },
+      {
         path: '',
         redirect: { name: 'main' }
       }
@@ -435,10 +439,6 @@ const routes = [
   {
     path: '/about',
     component: AboutPage
-  },
-  {
-    path: '/faq',
-    component: FaqPage
   },
   {
     path: '*',
