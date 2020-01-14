f6aff0d5790e80601d5c0eee6deb517cd4da4442
d.varava <d.varava@itech-group.ru>
2020-01-14 12:02:55 +0300

    GYRE-1222 fix

--- app/Helpers/FileHelpers.php
+++ app/Helpers/FileHelpers.php
@@ -3,33 +3,40 @@
 namespace App\Helpers;
 
 use App\Models\Track;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class FileHelpers
 {
     /**
      * @param $entity
+     *
+     * @return bool
      */
     public static function clearingStorage($entity)
     {
-        if ($entity instanceof Track) {
-            self::clearingTrackStorage($entity);
-        } elseif (null !== $entity->getImage()) {
+        try {
+            if ($entity instanceof Track) {
+                self::removeTrack($entity);
+            }
+
             self::removeByMask($entity->getImage());
+
+            return true;
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
         }
+
+        return false;
     }
 
     /**
      * @param Track $track
      */
-    public static function clearingTrackStorage(Track $track)
+    public static function removeTrack(Track $track)
     {
         if (false === empty($track->filename)) {
             Storage::disk('public')->delete($track->filename);
-        }
-
-        if (Storage::disk('public')->exists($track->cover)) {
-            self::removeByMask($track->getImage());
         }
     }
 
